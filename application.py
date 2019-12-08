eaae6a6eab6806b12654fe3f2dbb8a87c548243a
Dasmer Singh <dasmersingh@gmail.com>
2019-12-07 23:35:28 -0500

    Remove GET

--- application.py
+++ application.py
@@ -10,10 +10,10 @@ db.init_app(app)
 
 @app.route("/")
 def index():
-    response = requests.get(f'{request.url_root}api/notes')
-    print(f'BEGIN{response}END')
-    notes = response.json()
-    return render_template("index.html", notes = notes)
+    # response = requests.get(f'{request.url_root}api/notes')
+    # print(f'BEGIN{response}END')
+    # notes = response.json()
+    return render_template("index.html", notes = []])
 
 
 @app.route("/notes/<int:id>")
